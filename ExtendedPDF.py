258175f856f0ab97667b90f7b6fe4b757d4513f3
Cosmin Poieana <cosmin@robocorp.com>
2023-02-01 19:22:55 +0700

    Adds tasks for parsing various PDFs

---
+++ ExtendedPDF.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Optional
+
+import camelot
+from RPA.PDF import PDF
+from RPA.PDF.keywords import keyword
+
+
+class ExtendedPDF(PDF):
+
+    """Quickstart: https://camelot-py.readthedocs.io/en/master/user/quickstart.html?highlight=read_pdf"""
+
+    @keyword
+    def get_tables_from_pdf(self, source_path: Optional[str] = None, *args, **kwargs):
+        """Documentation: https://camelot-py.readthedocs.io/en/master/api.html?highlight=read_pdf#camelot.read_pdf"""
+
+        self.switch_to_pdf(source_path)
+
+        tables = camelot.read_pdf(
+            self.active_pdf_document.path, *args, **kwargs
+        )
+        return tables
+
+    @keyword
+    def export_tables_to_files(self, tables, output_path, output_format="csv", **kwargs):
+        """Documentation: https://camelot-py.readthedocs.io/en/master/api.html?highlight=export#camelot.core.TableList.export"""
+
+        # Supported formats: csv|excel|html|json|markdown|sqlite
+        tables.export(output_path, f=output_format, **kwargs)
+
+        # Returns all the exported files, which is one per table. (pages X tables)
+        path = Path(output_path)
+        pattern = f"{path.stem}*{path.suffix}"
+        return path.parent.glob(pattern)
